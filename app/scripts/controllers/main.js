21f19ef2b3be27117af0170d122024e874a88180
Guilherme Hott <guilhermehott@gmail.com>
2014-01-15 17:47:16 -0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('kickstartApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+        'SitePoint'
+    ];
+  });
